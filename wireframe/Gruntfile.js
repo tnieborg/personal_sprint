755c9c551426dd152623674974ccb2dc3933758c
tnieborg <twnieborg@gmail.com>
2017-09-18 08:33:26 +0200

    started on making the index and the css for the header

--- wireframe/Gruntfile.js
+++ wireframe/Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
             },
         css: {
                 files: ['./sass/**/*.scss'],
-                tasks: ['sass', 'cssmin'],
+                tasks: ['sass'],
             },
         }, 
         sass: {
